b4192ab16afbfb3ace1469aab8b5fe03b3d71c2c
ymk <ymg4798@naver.com>
2022-11-20 16:35:47 +0900

    백준 알고리즘 1932
    DP 문제였고 거꾸로 올라가는 방식
    향후 다시 풀어보자

--- src/Main.java
+++ src/Main.java
@@ -4,56 +4,5 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int arr[] = new int[n];
-
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int result = 0;
-        int start = 0;
-        int end = 0;
-        int limit[] = new int[100001];
-        limit[arr[start]]++;
-
-        for(int i = 1; i < n; i++){
-            if(limit[arr[i]] < k) {
-                end = i;
-                limit[arr[end]]++;
-                result = Math.max(end-start+1, result);
-            }else {
-                while(true) {
-                    if(arr[start] == arr[i]) {
-                        start++;
-                        end = i;
-                        break;
-                    } else {
-                        limit[arr[start]]--;
-                        start++;
-                    }
-                }
-            }
-        }
-        System.out.println(result);
-    }
 }
-/*
-  for(int j = i; j < n; j++) {
-        rtemp++;
-        temp[arr[j]]++;
-        if(temp[arr[j]] > k) {
-        result = Math.max(result, rtemp-1);
-        rtemp = 0;
-        } else if(j == n-1) {
-        result = Math.max(result, rtemp);
-        rtemp = 0;
-        }
-        }
-        temp = new int[100001]; 시간초과 ... */
