45f16640a218cc8dc2823ef7aeaee9bb5122123f
ymk <ymg4798@naver.com>
2023-11-17 23:24:38 +0900

    백준 큐 스택 덱 알고리즘

---
+++ src/stackqueuedeque/back_10773.java
@@ -0,0 +1,34 @@
+package stackqueuedeque;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class back_10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < k; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n != 0) {
+                stack.push(n);
+            } else {
+                stack.pop();
+            }
+        }
+
+        int result = 0;
+        if (!stack.isEmpty()) {
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
+                result += stack.pop();
+            }
+        }
+        System.out.println(result);
+    }
+}
--- src/stackqueuedeque/back_28278.java
+++ src/stackqueuedeque/back_28278.java
@@ -1,22 +1,16 @@
 package stackqueuedeque;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/**
- * 먼저 들어간게 맨위 정수
- */
 public class back_28278 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        Queue<Integer> queue = new LinkedList<>();
 
         int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -24,10 +18,33 @@ public class back_28278 {
             int command = Integer.parseInt(st.nextToken());
 
             if (command == 1) {
-                queue.add(Integer.parseInt(st.nextToken()));
+                stack.push(Integer.parseInt(st.nextToken()));
             } else if (command == 2) {
-
+                try {
+                    bw.append(String.valueOf(stack.pop()));
+                } catch (Exception e) {
+                    bw.append("-1");
+                }
+                bw.append("\n");
+            } else if (command == 3) {
+                bw.append(String.valueOf(stack.size()));
+                bw.append("\n");
+            } else if (command == 4) {
+                if (stack.isEmpty()) {
+                    bw.append("1");
+                } else {
+                    bw.append("0");
+                }
+                bw.append("\n");
+            } else if (command == 5) {
+                try {
+                    bw.append(String.valueOf(stack.peek()));
+                } catch (Exception e) {
+                    bw.append("-1");
+                }
+                bw.append("\n");
             }
         }
+        bw.flush();
     }
 }
