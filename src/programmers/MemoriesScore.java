d3eb20f87199e9228fa12591822689b2a38e4980
ymk <ymg4798@naver.com>
2023-11-22 23:40:03 +0900

    프로그래머스 알고리즘 김영한님 강의 웹서버와 서블릿 컨테이너 및 내장톰켓

--- src/programmers/MemoriesScore.java
+++ src/programmers/MemoriesScore.java
@@ -3,28 +3,28 @@ package programmers;
 import java.util.HashMap;
 
 public class MemoriesScore {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String, Integer> map = new HashMap<>();
 
-    public static void main(String[] args) {
-        String[] a = new String[1];
-
-
-        System.out.println(a[0].equals("a"));
-    }
-}
-    class Solution {
-        public int[] solution(String[] name, int[] yearning, String[][] photo) {
-            int[] answer = new int[photo.length];
-
-            HashMap<String, Integer> map = new HashMap<>();
-
-            for (int i = 0; i < name.length; i++) {
-                map.put(name[i], yearning[i]);
-            }
-
-            for (int i = 0; i < photo.length; i++) {
-                String[] valueArray = photo[i];
-            }
-
-            return answer;
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
         }
+
+        for (int i = 0; i < photo.length; i++) {
+            String[] valueArray = photo[i];
+
+            int sum = 0;
+            for (int j = 0; j < valueArray.length; j++) {
+                String value = valueArray[j];
+
+                if (map.containsKey(value)) {
+                    Integer point = map.get(value);
+                    sum += point;
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
 }
