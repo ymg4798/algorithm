0a805990371e6f3697f39e234d7789cde3e0a9bc
ymk <ymg4798@naver.com>
2023-11-24 21:57:04 +0900

    프로그래머스 알고리즘 김영한님 강의 웹서버와 서블릿 컨테이너 및 내장톰켓

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String args[]) {
-        String[][] a = {{"1", "2"}, {"2", "3"}};
+        int a = Integer.parseInt("-1");
+        int b = Integer.parseInt("-4");
 
-        String[] b = a[0];
-        String c = b[0]
-                ;
-        Stack<String[]> stack = new Stack<>();
-        System.out.println(stack.pop()[0]);
-
-
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        if (a > b) {
+            System.out.println(a);Math.ma 97 123z
         }
+
+        PriorityQueue
+
+
     }
 }
