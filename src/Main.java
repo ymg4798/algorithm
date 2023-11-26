873e5c59223582d5de658998c58518eee4f78112
ymk <ymg4798@naver.com>
2023-11-26 23:52:37 +0900

    프로그래머스 알고리즘 김영한님 강의 웹서버와 서블릿 컨테이너 및 내장톰켓

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,16 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String args[]) {
+        class Solution {
+            public int solution(int n) {
+                long[] dp = new long[100001];
 
+                dp[1] = 1;
+                for (int i = 2; i <= n; i++) {
+                    dp[i] = dp[i-1] + dp[i-2];
+                }
 
-    }
+                return (int) (dp[n] % 1234567L);
+            }
+        }
 }
