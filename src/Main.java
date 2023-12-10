5a3eb9a8b6582d13e26487800e6969bda05b21e8
ymk <ymg4798@naver.com>
2023-12-10 23:48:38 +0900

    프로그래머스 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,43 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String args[]) {
     }
+
+    class Solution {
+        public int solution(int[] priorities, int location) {
+            int answer = 0;
+            Queue<Task> q = new LinkedList<>();
+
+            for (int i = 0; i < priorities.length; i++) {
+                q.offer(new Task(priorities[i], i));
+            }
+
+            while(!q.isEmpty()) {
+                Task poll = q.poll();
+
+                if (poll.location == location) break;
+
+                for (Task task : q) {
+                    if (task.process > poll.process) {
+                        q.offer(poll);
+                        continue;
+                    }
+                }
+
+                answer++;
+            }
+
+
+            return answer;
+        }
+
+        class Task {
+            int process;
+            int location;
+
+            public Task(int process, int location) {
+                this.process = process;
+                this.location = location;
+            }
+        }
+    }
 }
---
+++ src/programmers/level3/Network.java
@@ -0,0 +1,34 @@
+package programmers.level3;
+
+import java.util.*;
+
+public class Network {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (visit[i]) {
+                continue;
+            }
+
+            q.offer(i);
+            visit[i] = true;
+
+            while (!q.isEmpty()) {
+                int poll = q.poll();
+
+                for (int j = 0; j < n; j++) {
+                    if (computers[poll][j] == 1 && !visit[j]) {
+                        q.offer(j);
+                        visit[j] = true;
+                    }
+                }
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
