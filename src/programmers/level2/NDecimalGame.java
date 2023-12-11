80c33208d9b88029869cd7f174e802a4dc9e4ad1
ymk <ymg4798@naver.com>
2023-12-11 23:51:41 +0900

    프로그래머스 알고리즘 원티드 프리 온보딩 도커

---
+++ src/programmers/level2/NDecimalGame.java
@@ -0,0 +1,26 @@
+package programmers.level2;
+
+public class NDecimalGame {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+
+        int num = 0;
+        int count = 0;
+        while(sb.length() < t) {
+            String str = Integer.toString(num, n);
+
+            for (char c : str.toCharArray()) {
+                count++;
+                if (count == p && sb.length() < t) {
+                    sb.append(Character.toUpperCase(c));
+                }
+                if (count == m) {
+                    count = 0;
+                }
+            }
+            num++;
+        }
+
+        return sb.toString();
+    }
+}
